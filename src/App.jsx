f944af027142b52fa76809383aeb81528b03c91b
Kevin Geejo <74069952+KevinGeejo@users.noreply.github.com>
2024-02-07 10:46:44 +0530

    Remove unnecessary files and update dependencies

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar';
+import Hero from './pages/Hero';
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+    </div>
+  );
+}
+
+export default App;
